a9ba6148674011aaa7eb2749a04bb96758ca607d
Yuan Zhi <zy8715@mun.ca>
2019-04-30 14:28:26 -0230

    set up for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -3,14 +3,13 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-//messageOne.textContent = 'From JS'
 
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
     messageTwo.textContent = 'Loading message...'
-    fetch(`http://localhost:3000/weather?address=${location}`)
+    fetch(`/weather?address=${location}`)
     .then((response)=>response.json().then(data => {
     if (data.error){
         messageTwo.textContent = 'error happens: ' + data.error
--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const hbs = require('hbs');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
-
 const app = express();
+const port = process.env.PORT || 3000;
 //Define the express configuration
 const publicDirectoryPath = path.join(__dirname, '../public');
 const viewPath = path.join(__dirname, '../templates/views');
@@ -93,6 +93,6 @@ app.get('*', (req, res) => {
         name: 'peter'
     })
 })
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port 3000' + port)
 })
